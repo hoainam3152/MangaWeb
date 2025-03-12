ae6528a4e8fac5396752f9368215e6b79aebc93a
hoainam3152 <hoainam3152@gmail.com>
2025-03-12 18:41:11 +0700

    add necessary packages

--- MangaAPI/MangaAPI/Program.cs
+++ MangaAPI/MangaAPI/Program.cs
@@ -1,3 +1,6 @@
+using MangaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(
+        builder.Configuration.GetConnectionString("AppDB")
+    ));
 
 var app = builder.Build();
 
