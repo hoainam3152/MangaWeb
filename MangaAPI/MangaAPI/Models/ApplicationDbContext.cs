8d63cd7837c54b653ecfa9d8156b4dd582647f0b
unknown <hoainam3152@gmail.com>
2025-03-17 13:25:14 +0700

    Create MangaDatabase

--- MangaAPI/MangaAPI/Models/ApplicationDbContext.cs
+++ MangaAPI/MangaAPI/Models/ApplicationDbContext.cs
@@ -10,5 +10,38 @@ namespace MangaAPI.Models
         {
             
         }
+
+        #region
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Manga> Mangas { get; set; }
+        public DbSet<MangaGenres> MangaGenres { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Default configurations for Identity entities, including IdentityUserLogin<string> 
+            base.OnModelCreating(modelBuilder);
+
+            //Create unique index for AuthorName
+            modelBuilder.Entity<Genre>()
+                .HasIndex(e => e.GenreName)
+                .IsUnique();
+
+            modelBuilder.Entity<MangaGenres>(entity =>
+            {
+                //Create PK
+                entity.HasKey(e => new { e.MangaId, e.GenreId });
+
+                //Create FK
+                entity.HasOne(e => e.Manga)
+                    .WithMany(e => e.MangaGenres)
+                    .HasForeignKey(e => e.MangaId);
+
+                entity.HasOne(e => e.Genre)
+                    .WithMany(e => e.MangaGenres)
+                    .HasForeignKey(e => e.GenreId);
+            });
+        }
     }
 }
---
+++ MangaAPI/MangaAPI/Models/Manga.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MangaAPI.Models
+{
+    [Tags("Manga")]
+    public class Manga
+    {
+        [Key]
+        public int MangaId { get; set; }
+        [StringLength(255)]
+        public string Title { get; set; }
+        public int? AuthorId { get; set; }
+        public string? Description { get; set; }
+        public int Status { get; set; }
+        [StringLength(255)]
+        public string? CoverImage { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author? Author { get; set; }
+
+        public ICollection<MangaGenres>? MangaGenres { get; set; }
+    }
+}
